a32c7adeb95b5e68a3cf33c3c5922afb43807c9e
19시_웹개발_heun <19__heun@DESKTOP-1P2Q3TM>
2021-02-09 19:53:00 +0900

    ㅇㅇ

---
+++ 02_java_array_basic/src/step2_01/array/ArrayEx01.java
@@ -0,0 +1,52 @@
+package step2_01.array;
+
+import java.util.Arrays;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1. 일반 변수 ( 값 저장 ) 
+				int nVar = 0;
+				nVar = 3;
+				System.out.println(nVar);       //값
+				
+				// 2. 주소 변수 ( 주소 저장 )
+				int[] arr = null;
+				arr = new int[5]; 
+				System.out.println(arr);        //주소
+				
+				
+				System.out.println(arr[0]);  //배열은 생성시 초깃값이 셋팅이 되어있다.
+				System.out.println(arr[1]);  //정수형태의 배열은 0 , 나머지는 직접 확인 해보자. 
+				System.out.println(arr[2]);
+				System.out.println(arr[3]);
+				System.out.println(arr[4]);
+				System.out.println();
+				
+			   // System.out.println(arr[50]);
+			   // java.lang.ArrayIndexOutOfBoundsException:5
+		       //배열의 범위가 넘어설때 나는 에러메세지 (해당 에러메세지 암기)
+		
+		
+			   arr[0] = 10;
+			   arr[1] = 20;
+			   arr[2] = 30;
+			   arr[3] = 40;
+			   arr[4] = 50;
+			   
+				for (int i = 0; i < arr.length; i++) {	// arr.length > 배열의 크기
+					System.out.println(arr[i]);
+				}
+				System.out.println();
+				
+				// (참고) 배열이 익숙해진 후에 사용
+				System.out.println(Arrays.toString(arr));
+				
+			    arr = null; // 배열 전체 삭제
+			   
+			       
+	}
+
+}
